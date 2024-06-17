e90d1203bee0b8c248fb6ea33bd7affcd25b303d
pprobst <pprobst@insiberia.net>
2024-06-17 13:26:41 -0300

    fix: openai requisition

--- text/gen.py
+++ text/gen.py
@@ -11,24 +11,21 @@ from tenacity import (
 MAX_TOKENS = {
     # https://platform.openai.com/docs/models/gpt-4
     # https://platform.openai.com/docs/models/gpt-3-5
-    "gpt-4": 8192,
-    "gpt-4-32k": 32768,
-    "gpt-3.5-turbo": 4097,
-    "gpt-3.5-turbo-16k": 16385,
+    "gpt-4-turbo": 128000,
+    "gpt-3.5-turbo-0125": 16385,
 }
 
 
 # https://github.com/openai/openai-cookbook/blob/main/examples/How_to_handle_rate_limits.ipynb
 @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
 def make_chatgpt_query(
-    client, query: str, return_type: str, model: str = "gpt-3.5-turbo"
+    client, query: str, return_type: str, model: str = "gpt-3.5-turbo-0125"
 ) -> List[str]:
     """
     Makes a query to the ChatGPT model and returns the generated response.
 
     Args:
         query (str): The user's query.
-        api_key (str): The API key for accessing the ChatGPT model.
         model (str): The name of the ChatGPT model.
 
     Returns:
@@ -39,11 +36,8 @@ def make_chatgpt_query(
         model=model, messages=[{"role": "user", "content": query}]
     )
     is_truncated = response.usage.total_tokens >= max_tokens
-    response = [
-        line
-        for line in response.choices[0].message.content.split("\n")
-        if line.strip() != ""
-    ]
-    if is_truncated and len(response) > 0:
-        response.pop()
-    return response
+    response_text = response.choices[0].message.content
+    response_lines = [line for line in response_text.split("\n") if line.strip() != ""]
+    if is_truncated and len(response_lines) > 0:
+        response_lines.pop()
+    return response_lines
